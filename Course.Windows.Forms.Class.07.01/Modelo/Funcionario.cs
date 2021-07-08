65677fb5991fcffd9ded972b097b1f7608838a94
marcus bianchi <marcusbianchi92@hotmail.com>
2021-07-08 19:50:28 -0300

    Adicionar arquivos de projeto.

---
+++ Course.Windows.Forms.Class.07.01/Modelo/Funcionario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Course.Windows.Forms.Class._07._01.Modelo {
+    class Funcionario {
+        public int Id { get; set; }
+        public string MyProperty { get; set; }
+        public string Email { get; set; }
+        public decimal Salario { get; set; }
+        public string Sexo { get; set; }
+        public string TipoContrato { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DateAtualizacao { get; set; }
+
+    }
+}
---
+++ Course.Windows.Forms.Class.07.01/TelaPrincipal.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace Course.Windows.Forms.Class._07._01 {
+    partial class TelaPrincipal {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.botaoEditar = new System.Windows.Forms.Button();
+            this.botaoExcluir = new System.Windows.Forms.Button();
+            this.tabelaFuncionario = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.tabelaFuncionario)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(12, 507);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(156, 67);
+            this.btnNovo.TabIndex = 0;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            // 
+            // botaoEditar
+            // 
+            this.botaoEditar.Location = new System.Drawing.Point(448, 507);
+            this.botaoEditar.Name = "botaoEditar";
+            this.botaoEditar.Size = new System.Drawing.Size(155, 67);
+            this.botaoEditar.TabIndex = 1;
+            this.botaoEditar.Text = "Editar";
+            this.botaoEditar.UseVisualStyleBackColor = true;
+            // 
+            // botaoExcluir
+            // 
+            this.botaoExcluir.Location = new System.Drawing.Point(622, 507);
+            this.botaoExcluir.Name = "botaoExcluir";
+            this.botaoExcluir.Size = new System.Drawing.Size(137, 67);
+            this.botaoExcluir.TabIndex = 2;
+            this.botaoExcluir.Text = "Excluir";
+            this.botaoExcluir.UseVisualStyleBackColor = true;
+            // 
+            // tabelaFuncionario
+            // 
+            this.tabelaFuncionario.AllowUserToAddRows = false;
+            this.tabelaFuncionario.AllowUserToDeleteRows = false;
+            this.tabelaFuncionario.AllowUserToResizeRows = false;
+            this.tabelaFuncionario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tabelaFuncionario.Location = new System.Drawing.Point(26, 12);
+            this.tabelaFuncionario.Name = "tabelaFuncionario";
+            this.tabelaFuncionario.Size = new System.Drawing.Size(764, 489);
+            this.tabelaFuncionario.TabIndex = 3;
+            this.tabelaFuncionario.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // TelaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 586);
+            this.Controls.Add(this.tabelaFuncionario);
+            this.Controls.Add(this.botaoExcluir);
+            this.Controls.Add(this.botaoEditar);
+            this.Controls.Add(this.btnNovo);
+            this.Name = "TelaPrincipal";
+            this.Text = "Tela principal";
+            ((System.ComponentModel.ISupportInitialize)(this.tabelaFuncionario)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button botaoEditar;
+        private System.Windows.Forms.Button botaoExcluir;
+        private System.Windows.Forms.DataGridView tabelaFuncionario;
+    }
+}
+

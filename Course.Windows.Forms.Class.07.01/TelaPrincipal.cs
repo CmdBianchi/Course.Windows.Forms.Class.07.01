e37709d09fee68c7ed58248c0b7dfce3c112e7c1
marcus bianchi <marcusbianchi92@hotmail.com>
2021-07-12 15:04:55 -0300

    Commit 03 Banco criado com sucesso.

--- Course.Windows.Forms.Class.07.01/TelaPrincipal.cs
+++ Course.Windows.Forms.Class.07.01/TelaPrincipal.cs
@@ -7,15 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.;
 namespace Course.Windows.Forms.Class._07._01 {
     public partial class TelaPrincipal : Form {
         public TelaPrincipal() {
             InitializeComponent();
+            dvgTabelaFucnionario.DataSource = Banco
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) {
-
+            InitializeComponent();       
         }
     }
 }
---
+++ Course.Windows.Forms.Class.07.01/_Banco/ionarioDataAccess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlServerCe;
+using System.Data;
+
+namespace Banco {
+    class ionarioDataAccess {
+        public static DataTable PegarFuncionarios() {
+            SqlCeConnection con = new SqlCeConnection(@"Data Source=C:\Users\marcu\source\repos\Course.Windows.Forms.Class.07.01\Course.Windows.Forms.Class.07.01\_Banco\Banco03.db");
+            SqlCeDataAdapter da = new SqlCeDataAdapter("SELECT * FROM Fucnioanrio", con);
+            DataSet ds = new DataSet();
+
+            da.Fill(ds);
+            return ds.Tables[0];
+        }
+    }
+}
